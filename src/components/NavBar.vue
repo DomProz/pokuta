450816a7567b1d7e6e83d46b1d1b9240d426e701
Szymon Sowierszenko <rambotracer@gmail.com>
2024-01-18 18:48:00 +0100

    router + home

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Post Blog Guns!</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/post/create" class="nav-link">Create Post</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/post/list" class="nav-link">Posts List</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,20 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home.vue";
+import CreatePost from "./components/CreatePost.vue";
+import ListPost from "./components/ListPost.vue";
+import * as VueRouter from 'vue-router'
 
-createApp(App).mount("#app");
+const routes = [
+  { path: "/", component: Home },
+  { path: "/post/create", component: CreatePost },
+  { path: "/post/list", component: ListPost },
+];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount("#app");
